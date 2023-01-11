47a3b701cb7664901bced94d0c0024d8e7a99796
rodijanssen <74348802+rodijanssen@users.noreply.github.com>
2023-01-11 09:58:27 +0100

    fix login

--- Functions/main_functions.php
+++ Functions/main_functions.php
@@ -1,16 +1,19 @@
 <?php
 require '../Connection/conn.php';
 
-class User {
+class User
+{
     private $mysqli;
     public $query;
-    
+
     // Constructor
-    public function __construct($mysqli) {
-    $this->mysqli = $mysqli;
+    public function __construct($mysqli)
+    {
+        $this->mysqli = $mysqli;
     }
 
-    public function create($Voornaam, $Achternaam, $Groep, $Wachtwoord, $Rechten) {
+    public function create($Voornaam, $Achternaam, $Groep, $Wachtwoord, $Rechten)
+    {
         // Insert the new item into the database
         $stmt = $this->mysqli->prepare("INSERT INTO gebruikers (Voornaam, Achternaam, Groep, Wachtwoord, Rechten) VALUES (?, ?, ?, ?, ?)");
         $stmt->bind_param("sssss", $Voornaam, $Achternaam, $Groep, $Wachtwoord, $Rechten);
@@ -18,28 +21,31 @@ class User {
         $stmt->close();
     }
 
-    public function read($datatable) {
+    public function read($datatable)
+    {
         // Select all data from the users table
         $stmt = $this->mysqli->prepare("SELECT * FROM " . $datatable);
-        $stmt->execute();       
+        $stmt->execute();
         $result = $stmt->get_result(); // Bind the result to a new variable
         $data = $result->fetch_all(MYSQLI_ASSOC); // Fetch all rows as an associative array
         $stmt->close(); // Close the statement
         return $data; // Return the data
-        }
-        
-    public function filtersearch($datatable, $datarow, $search) {
-            // Select all data from the users table
+    }
+
+    public function filtersearch($datatable, $datarow, $search)
+    {
+        // Select all data from the users table
         $stmt = $this->mysqli->prepare("SELECT * FROM " . $datatable . " where " . $datarow . " = ?");
         $stmt->bind_param("s", $search);
-        $stmt->execute();       
+        $stmt->execute();
         $result = $stmt->get_result(); // Bind the result to a new variable
         $data = $result->fetch_all(MYSQLI_ASSOC); // Fetch all rows as an associative array
         $stmt->close(); // Close the statement
         return $data; // Return the data
-        }    
+    }
 
-    public function randomPassword() {
+    public function randomPassword()
+    {
         $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
         $pass = array(); //remember to declare $pass as an array
         $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
@@ -50,22 +56,22 @@ class User {
         return implode($pass); //turn the array into a string
     }
 
-    public function login($Voornaam, $Wachtwoord) {
+    public function login($Voornaam, $Wachtwoord)
+    {
         // Check the database to see if there is a user with the given username and password
-        $stmt = $this->mysqli->prepare("SELECT * FROM gebruikers WHERE Voornaam = ? AND Wachtwoord = ?");
+        $stmt = $this->mysqli->prepare("SELECT COUNT(*) as user_count FROM gebruikers WHERE Voornaam = ? AND Wachtwoord = ?");
         $stmt->bind_param("si", $Voornaam, $Wachtwoord);
         $stmt->execute();
         $result = $stmt->get_result(); // Bind the result to a new variable
         $user = $result->fetch_assoc(); // Fetch the first row as an associative array
         $stmt->close(); // Close the statement
-      
+
         // If the user was found, return true
-        if ($user) {
-          return true;
+        if ($user['user_count'] > 0) {
+            return true;
         }
-      
+
         // If the user was not found, return false
         return false;
     }
 }
-?>
--- Pages/login.php
+++ Pages/login.php
@@ -1,44 +1,44 @@
 <?php
-    include '../Functions/main_functions.php';
-    include '../Assets/layout.php';
+include '../Functions/main_functions.php';
+include '../Assets/layout.php';
 
-    session_start();
-    $gebruiker =  new user($mysqli);
+session_start();
+$gebruiker =  new user($mysqli);
 
-    if(isset($_POST['Login']))
-    {
-        if($gebruiker->login($_POST['Voornaam'], $_POST['Wachtwoord']))
-        {
-            $_SESSION['Voornaam'] = $_POST['Voornaam'];
-            echo "<script type='text/javascript'> window.location.href='filter.php'</script>";
-        }
-        {
-            echo "Er ging iets fout!";
-        }
+if (isset($_POST['Login'])) {
+    if ($gebruiker->login($_POST['Voornaam'], $_POST['Wachtwoord'])) {
+        $_SESSION['Voornaam'] = $_POST['Voornaam'];
+        echo "<script type='text/javascript'> window.location.href='filter.php'</script>";
+    } else {
+        echo "Er ging iets fout!";
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
+
 <body>
-<div class="container">
-            <form name="Registreren" style="margin: auto; width: 400px;" method="POST" action="login.php" autocomplete="off">
-                <div class="mb-3">
-                    <label for="VoorbeeldVoornaam1" class="form-label">Voornaam</label>
-                    <input type="Text" name="Voornaam" class="form-control" aria-describedby="VoornaamHelp" required>
-                </div>
-                <div class="mb-3">
-                    <label for="VoorbeelWachtwoord1" class="form-label">Wachtwoord</label>
-                    <input type="Text" name="Wachtwoord" class="form-control" aria-describedby="WachtwoordHelp" required>
-                </div>
-                    <button type="submit" name="Login" class="btn btn-primary">Login</button>
-            </form>
+    <div class="container">
+        <form name="Registreren" style="margin: auto; width: 400px;" method="POST" action="login.php" autocomplete="off">
+            <div class="mb-3">
+                <label for="VoorbeeldVoornaam1" class="form-label">Voornaam</label>
+                <input type="Text" name="Voornaam" class="form-control" aria-describedby="VoornaamHelp" required>
+            </div>
+            <div class="mb-3">
+                <label for="VoorbeelWachtwoord1" class="form-label">Wachtwoord</label>
+                <input type="Text" name="Wachtwoord" class="form-control" aria-describedby="WachtwoordHelp" required>
+            </div>
+            <button type="submit" name="Login" class="btn btn-primary">Login</button>
+        </form>
     </div>
 </body>
+
 </html>
